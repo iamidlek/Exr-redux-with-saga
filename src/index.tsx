ce370fecb8639597ec31c033927c1c0ba00cfaf2
iamidlek <iamidlek@naver.com>
2022-01-18 23:33:31 +0900

    feat: create redux store, axios base URL

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import axios from "axios";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
+
+// 같은 origin의 경우 request header 에 cookie가 추가되는데
+// orgin이 달라지는 경우 자동으로 추가되지 않아 생기는 문제를 해결 하기 위한 조치
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
